4cc9339a0a0d09b618dae4da571b96c4a3a5cd10
Cuong Duy Nguyen <103cuong@gmail.com>
2020-04-12 16:55:37 +0700

    Hello Minikube (#1)

---
+++ hello_minikube/server.js
@@ -0,0 +1,13 @@
+minikube / server.js
+
+var http = require('http');
+
+var handleRequest = function (request, response) {
+    console.log('Received request for URL: ' + request.url);
+    response.writeHead(200);
+    response.end('Hello World!');
+};
+
+var www = http.createServer(handleRequest);
+
+www.listen(8080);
