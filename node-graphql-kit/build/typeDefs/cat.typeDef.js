eeb35b1d5586b0e35ee99bb876a0192effa7b3ee
Cuong Duy Nguyen <103cuong@gmail.com>
2020-06-23 23:04:28 +0700

    Remove build folder

--- node-graphql-kit/build/typeDefs/cat.typeDef.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var typeDef = "\n  type Query {\n    cats(id: String): [Cat]\n  }\n\n  type Mutation {\n    createCat(name: String!, color: String, categoryId: String!): Cat\n  }\n\n  type Cat {\n    id: String\n    name: String\n    color: String\n    categoryId: String\n    category: Category\n    createdAt: DateTime\n    updatedAt: DateTime\n  }\n";
-var _default = typeDef;
-exports["default"] = _default;
